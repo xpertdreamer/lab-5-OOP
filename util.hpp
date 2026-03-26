77e10dbcb48275909929551903f25e860d997993
xpertdreamer <yungnomen@gmail.com>
2026-03-26 21:40:39 +0300

    Added new file for utils

---
+++ util.hpp
@@ -0,0 +1,55 @@
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#include "cars.hpp"
+#include <limits>
+#include <list>
+#include <sstream>
+#include <string>
+#include <vector>
+
+void fill_list(std::list<Car> &cars) {
+    int n;
+    std::cin >> n;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+    for (int i = 0; i < n; i++) {
+        std::string _brand, _model;
+        int _year;
+        double _price;
+
+        std::cout << "Enter data for " << i + 1 << " >\n";
+        std::cout << "Brand > ";
+        std::getline(std::cin, _brand);
+        std::cout << "Model > ";
+        std::getline(std::cin, _model);
+        std::cout << "Year >";
+        std::cin >> _year;
+        std::cout << "Price > ";
+        std::cin >> _price;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        cars.push_back(Car(_brand, _model, _year, _price));
+    }
+}
+
+std::vector<int> input_indicies(int maxIdx) {
+    std::cout << "Enter indicies separated by space > ";
+    std::string input;
+    std::getline(std::cin, input);
+
+    std::vector<int> res;
+    std::stringstream ss;
+    int idx;
+
+    while (ss >> idx) {
+        if (idx >= 0 && idx < maxIdx)
+            res.push_back(idx);
+        else
+            std::cout << "Warning: Index " << idx
+                      << " will be ignored (out-of-range)\n";
+    }
+
+    return res;
+}
+#endif // UTIL_H_
